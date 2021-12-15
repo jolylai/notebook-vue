4dafc24f60ff82c889d214bde162592879955b23
jolylai <jolylai163@163.com>
2021-12-15 17:34:41 +0800

    feat: matcher

--- packages/vue-router-next/src/matcher/index.js
+++ packages/vue-router-next/src/matcher/index.js
@@ -1,4 +1,7 @@
-export function createMatcher(routes, globalOptions) {
+export function createRouterMatcher(routes, globalOptions) {
+  // normalized ordered array of matchers
+  const matchers = []
+
   const matcherMap = new Map()
 
   function getRecordMatcher(name) {
@@ -6,18 +9,61 @@ export function createMatcher(routes, globalOptions) {
   }
 
   /**
-   *
-   * @param {*} record
-   * @param {*} parent
+   * 新增路由
+   * @param {*} record 用户配置的路由
+   * @param {*} parent 父节点
    * @param {*} originalRecord
    */
   function addRoute(record, parent, originalRecord) {
     const normalizedRecords = [record]
 
+    // 当前路由匹配器
+    let matcher
+    let originalMatcher
+
     for (let normalizedRecord of normalizedRecords) {
       const { path } = normalizedRecord
+
+      matcher = createRouterMatcher(normalizedRecord)
+
+      if ('children' in normalizedRecord) {
+        const children = normalizedRecord.children
+        for (let i = 0; i < children.length; i++) {
+          addRoute(
+            children[i],
+            matcher,
+            originalRecord && originalRecord.children[i]
+          )
+        }
+      }
+
+      // if there was no original record, then the first one was not an alias and all
+      // other alias (if any) need to reference this record when adding children
+      originalRecord = originalRecord || matcher
     }
   }
 
-  return { getRecordMatcher }
+  //
+  function insertMatcher(matcher) {}
+
+  function resolve() {}
+  function removeRoute() {}
+  function getRoutes() {
+    return matchers
+  }
+
+  return { addRoute, resolve, removeRoute, getRoutes, getRecordMatcher }
 }
+
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About }
+]
+
+const matcher = createMatcher(routes)
